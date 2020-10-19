b954409c8ba00d581b8ee77eb2526e0d7bf3563a
Piplo31 <juan.pablo.salgado@correounivalle.edu.co>
2020-10-18 19:14:15 -0500

    api-rest sin html

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
+const cors = require("cors")
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
+app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')));
 
 const dbRoutes = require('./routers');
@@ -27,6 +28,6 @@ app.post('/guardar', (req, res) => {
 
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
