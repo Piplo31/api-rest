12fae6a4045d28af2b9f8bc29f7fb707d06682c5
Piplo31 <juan.pablo.salgado@correounivalle.edu.co>
2020-10-18 20:15:21 -0500

    Cambio diminuto

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-app.use(cors())
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const dbRoutes = require('./routers');
